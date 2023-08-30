381235fdd84671b9b615d470f515b802c92a1dd7
DavyHaYehoudi <davyhayehoudi@gmail.com>
2023-08-30 12:17:07 +0200

    ajout de la politique des orignes croisees

--- API/app.js
+++ API/app.js
@@ -1,16 +1,17 @@
 const express = require("express");
+const app = express();
 const morgan = require("morgan");
+const cors = require("cors")
 require("dotenv").config();
 const bodyParser = require("body-parser");
 const Sequelize = require("./src/db/sequelize");
-const app = express();
 const port = process.env.PORT || 3000;
 
 const pokemonRouter = require("./src/routes/pokemons.routes");
 const userRouter = require("./src/routes/user.routes");
 const errorHandler = require("./src/errors/errorHandler");
 
-app.use(morgan("dev")).use(bodyParser.json());
+app.use(morgan("dev")).use(bodyParser.json()).use(cors());
 Sequelize.initDb();
 
 app.use("/api/pokemons", pokemonRouter);
