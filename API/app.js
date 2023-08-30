f03c2b4c7f23314400291cb40e17c018bca7cd16
DavyHaYehoudi <davyhayehoudi@gmail.com>
2023-08-30 11:59:20 +0200

    scripts package.json + process.env.PORT dans app.js

--- API/app.js
+++ API/app.js
@@ -4,25 +4,25 @@ require("dotenv").config();
 const bodyParser = require("body-parser");
 const Sequelize = require("./src/db/sequelize");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-const pokemonRouter = require("./src/routes/pokemons.routes")
+const pokemonRouter = require("./src/routes/pokemons.routes");
 const userRouter = require("./src/routes/user.routes");
 const errorHandler = require("./src/errors/errorHandler");
 
 app.use(morgan("dev")).use(bodyParser.json());
-Sequelize.initDb()
+Sequelize.initDb();
 
-app.use("/api/pokemons",pokemonRouter)
-app.use("/api/auth",userRouter)
+app.use("/api/pokemons", pokemonRouter);
+app.use("/api/auth", userRouter);
 
 // Gestion des erreurs 404
-app.use(({res})=>{
-  const message = "Impossible de trouver la ressource demandée. Vous pouvez essayer une autre URL"
-  res.status(404).json({message})
-})
-app.use(errorHandler)
+app.use(({ res }) => {
+  const message =
+    "Impossible de trouver la ressource demandée. Vous pouvez essayer une autre URL";
+  res.status(404).json({ message });
+});
+app.use(errorHandler);
 app.listen(port, () =>
   console.log(`Notre application est démarée sur : http://localhost:${port}`)
 );
-
